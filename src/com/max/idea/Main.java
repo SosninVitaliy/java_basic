7b495836c6d88d1562576cd15e72ee5e4c8cae45
User <Vitaliy.Sosnin@appline.ru>
2022-05-15 02:39:02 +0300

    added vars

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -3,5 +3,11 @@ package com.max.idea;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+//      Переменная типа с плавающей точкой с наибольшим диапазоном чисел. Присвоено произвольное дробное значение
+        double d = 75457.34;
+//      Переменная целочисленного типа с наименьшим диапазоном чисел. Присвоено наибольшее возможное значение
+        byte b = 127;
+        System.out.println(d);
+        System.out.print(b);
     }
 }
