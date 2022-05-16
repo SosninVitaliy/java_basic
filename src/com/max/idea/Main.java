85e7653a8a27ee8de3f47f9ac00e172449796be6
User <Vitaliy.Sosnin@appline.ru>
2022-05-16 17:11:19 +0300

    task4

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -1,7 +1,9 @@
 package com.max.idea;
 
+
 import java.net.SocketOption;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -26,5 +28,30 @@ public class Main {
 //      Вывод в консоль результата суммы первого и среднего элемента
         int sum = nums[0] + nums[nums.length / 2 ];
         System.out.println(sum);
+
+//      Задача №4_Операторы
+        Scanner in = new Scanner(System.in);
+
+//      Ввести три числа с клавиатуры x,y,z
+        System.out.println("Input x: ");
+        double x = in.nextInt();
+
+        System.out.println("Input y: ");
+        double y = in.nextInt();
+
+        System.out.println("Input z: ");
+        double z = in.nextInt();
+
+//      Вывести в консоль среднее арифметическое чисел
+        double arithmeticMean = (x + y + z) / 3;
+        System.out.println(arithmeticMean);
+
+//      Разделить среднее арифметическое на 2 и округлить в меньшую сторону
+        int arithmeticMeanRounding = (int)Math.floor(arithmeticMean / 2);
+//      Если полученное число больше 3, то вывести на экран сообщение "Программа выполнена корректно"
+        if (arithmeticMeanRounding > 3) {
+            System.out.println("Программа выполнена корректно");
+        }
+        System.out.println(arithmeticMeanRounding);
     }
 }
