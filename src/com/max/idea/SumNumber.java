a05d09a25c4bf9727ca7287bc2914b78771a8892
User <Vitaliy.Sosnin@appline.ru>
2022-05-26 19:27:27 +0300

    task_8_cycles

---
+++ src/com/max/idea/SumNumber.java
@@ -0,0 +1,26 @@
+package com.max.idea;
+
+import java.util.Scanner;
+
+class SumNumber {
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num;
+        int sum = 0;
+        do {
+            System.out.println("Введите любое целое положительное число: ");
+            while (!input.hasNextInt()) {
+                System.out.println("Это не число!");
+                input.next();
+            }
+            num = input.nextInt();
+        } while (num <= 0);
+            for (int i = 0; i <= num; i++) {
+                if (i % 2 != 0) {
+                    sum += i;
+                }
+            }
+            System.out.println("Сумма нечетных цифр до введенного пользователем числа n: " + sum);
+    }
+}
