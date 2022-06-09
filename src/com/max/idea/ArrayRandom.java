0df0e7a4e860c2bee3bfc818d7f4ecb751fe663a
User <Vitaliy.Sosnin@appline.ru>
2022-06-10 00:26:35 +0300

    task_14

--- src/com/max/idea/ArrayRandom.java
+++ src/com/max/idea/ArrayRandom.java
@@ -1,4 +1,28 @@
 package com.max.idea;
 
+import java.util.Random;
+
 public class ArrayRandom {
+
+    static double[] array = new double[15];
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ((double) 20 + (random.nextDouble() * (-20 - 20)));
+        }
+        double max, min;
+        min = max = array[0];
+        for (double v : array) {
+            if (v > max) {
+                max = v;
+            } else if (v < min) {
+                min = v;
+            }
+        }
+        System.out.println("Максимальный элемент : " + max);
+        System.out.println("Минимальный элемент : " + min);
+        System.out.println("Максимальный элемент по модулю из минимального и максимального элементов: " + (Math.max(Math.abs(max), Math.abs(min))));
+    }
 }
